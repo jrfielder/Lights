7f95f6c8715c105041429e32aa07e7f26a2574c6
John Fielder <jrfielder08@gmail.com>
2024-05-02 13:57:06 -0400

    Working

--- controler.py
+++ controler.py
@@ -1,6 +1,6 @@
 import io
 import time
-import picamera2
+from picamera2 import Picamera2
 import numpy as np
 from scipy.ndimage import gaussian_filter
 import imageio
@@ -9,7 +9,7 @@ import cv2
 
 # Preprocessing function with sigma value for testing gaussian smoothing
 def process_frame(frame, sigma=1):
-    # grayscalePpp
+    # grayscale
     gray = np.dot(frame[..., :3], [0.2989, 0.5870, 0.1140])
     
     # Gaussian smoothing
@@ -24,28 +24,23 @@ def process_frame(frame, sigma=1):
 # Video handling
 def capture_frames():
     # Initialize the camera
-    with picamera2.PiCamera() as camera:
-        camera.framerate = 30
-        camera.resolution = (640, 480)
-        time.sleep(2) 
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+    picam2.start()
+
+    while True:
+        frame = picam2.capture_array()
+        processed_frame = process_frame(frame)
+
+        cv2.imshow("Camera", frame)
+        cv2.waitKey(1)
+
+
+        # Display the frame using OpenCV
+        cv2.imshow('Processed Frame', processed_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         
-		# initialize stream
-        stream = picamera2.array.PiRGBArray(camera, size=(640, 480))
-        
-        for _ in camera.capture_continuous(stream, format="bgr", use_video_port=True):
-            # Convert the image to a NumPy array and process it
-            frame = stream.array
-            processed_frame = process_frame(frame)
-            
-            # Display the frame using OpenCV
-            cv2.imshow('Processed Frame', processed_frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            
-            # Clear the stream in preparation for the next frame
-            stream.truncate(0)
-        
-        cv2.destroyAllWindows()
 
 # Start capturing frames
 capture_frames()
